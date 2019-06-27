f0895e9a801d6f49cd1d21d9e62d1d461d9d89ff
Eric Wallace <eswallace@comcast.net>
2019-06-27 10:45:36 -0700

    add sentiment analysis

--- allennlp/predictors/biattentive_classification.py
+++ allennlp/predictors/biattentive_classification.py
@@ -1,38 +1,36 @@
+from overrides import overrides
 from typing import Dict, List
 import numpy
-from overrides import overrides
 from allennlp.common.util import JsonDict
 from allennlp.data import Instance
 from allennlp.predictors.predictor import Predictor
+from allennlp.data.fields import LabelField
+from allennlp.data.tokenizers.word_tokenizer import WordTokenizer
 
-
-@Predictor.register('biattentive-classification')
-class BiattentiveClassificationPredictor(Predictor):
+@Predictor.register('sentiment-analysis')
+class TextClassifierPredictor(Predictor):
     """
-    Predictor for the :class:`~allennlp.models.BiattentiveClassificationNetwork` model.
+    Predictor for any model that takes in a sentence and returns
+    a single class for it.  In particular, it can be used with
+    the :class:`~allennlp.models.basic_classifier.BasicClassifier` model
     """
-
-    def predict(self, sentence: str) -> JsonDict:
-        """
-        Predicts the sentiment of the input text.
-
-        Parameters
-        ----------
-        sentence : ``str``
-            The input text.
-        
-        Returns
-        -------
-        A dictionary where the key "class_probabilities" determines the probabilities of each
-        of the sentiment labels [0,1,2,3,4].
-        """
-        return self.predict_json({"sentence" : sentence})
+    def predict(self, tokens: str) -> JsonDict:
+        return self.predict_json({"tokens": tokens})
 
     @overrides
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
         """
-        Expects JSON that looks like ``{"sentence": "..."}``.
+        Expects JSON that looks like ``{"tokens": "..."}``.
+        Runs the underlying model, and adds the ``"label"`` to the output.
         """                
-        input_text = json_dict["sentence"]        
-        input_text = input_text.split(' ')        
-        return self._dataset_reader.text_to_instance(input_text)
+        tokens = json_dict["tokens"]        
+        tokenizer = WordTokenizer()
+        tokens = [str(t) for t in tokenizer.tokenize(tokens)]        
+        return self._dataset_reader.text_to_instance(tokens)
+
+    @overrides        
+    def predictions_to_labeled_instances(self, instance: Instance, outputs: Dict[str, numpy.ndarray]) -> List[Instance]:        
+        label = numpy.argmax(outputs['class_probabilities'])
+        instance.add_field('label', LabelField(int(label), skip_indexing=True))                    
+        return [instance]
+
--- allennlp/predictors/text_classifier.py
+++ allennlp/predictors/text_classifier.py
@@ -1,35 +1,25 @@
 from overrides import overrides
-from typing import Dict, List
-import numpy
+
 from allennlp.common.util import JsonDict
 from allennlp.data import Instance
 from allennlp.predictors.predictor import Predictor
-from allennlp.data.fields import LabelField
-from allennlp.data.tokenizers.word_tokenizer import WordTokenizer
 
-@Predictor.register('sentiment-analysis')
+
+@Predictor.register('text_classifier')
 class TextClassifierPredictor(Predictor):
     """
     Predictor for any model that takes in a sentence and returns
     a single class for it.  In particular, it can be used with
     the :class:`~allennlp.models.basic_classifier.BasicClassifier` model
     """
-    def predict(self, tokens: str) -> JsonDict:
-        return self.predict_json({"tokens": tokens})
+    def predict(self, sentence: str) -> JsonDict:
+        return self.predict_json({"sentence": sentence})
 
     @overrides
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
         """
-        Expects JSON that looks like ``{"tokens": "..."}``.
+        Expects JSON that looks like ``{"sentence": "..."}``.
         Runs the underlying model, and adds the ``"label"`` to the output.
-        """                
-        tokens = json_dict["tokens"]        
-        tokenizer = WordTokenizer()
-        tokens = [str(t) for t in tokenizer.tokenize(tokens)]        
-        return self._dataset_reader.text_to_instance(tokens)
-
-    @overrides        
-    def predictions_to_labeled_instances(self, instance: Instance, outputs: Dict[str, numpy.ndarray]) -> List[Instance]:        
-        label = numpy.argmax(outputs['probs'])
-        instance.add_field('label', LabelField(int(label), skip_indexing=True))                    
-        return [instance]
+        """
+        sentence = json_dict["sentence"]
+        return self._dataset_reader.text_to_instance(sentence)
